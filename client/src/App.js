0be3ed1bd7050aec58138577583b612d1839040b
BEPUL-CSE-RU <bepul.cse.ru@gmail.com>
2020-01-11 18:09:36 +0600

    authectication practice

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import React, {Component} from "react"
+//import logo from './logo.svg';
+import './App.css';
+
+import Navbar from "./components/layout/Navbar"
+import Landing from "./components/layout/Landing"
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h>successfull</h>
+    </div>
+  );
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://Bepul:Bepul@cluster0-lo1zs.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "Bepul"
+};
