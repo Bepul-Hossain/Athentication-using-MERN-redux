b3a0bbc157099729c9a6029d8207f332bb6d5521
bepul0 <bepul.cse.ru@gmail.com>
2020-08-10 11:22:10 +0600

    ok

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb+srv://Bepul:Bepul@cluster0-lo1zs.mongodb.net/test?retryWrites=true&w=majority",
+  mongoURI: "mongodb://Bepul:Bepul@cluster0-shard-00-00-lo1zs.mongodb.net:27017,cluster0-shard-00-01-lo1zs.mongodb.net:27017,cluster0-shard-00-02-lo1zs.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
   secretOrKey: "Bepul"
 };
+//mongodb+srv://Bepul:Bepul@cluster0-lo1zs.mongodb.net/test?retryWrites=true&w=majority
